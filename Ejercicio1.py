ae63473a7fa034a4a1b2591e799b510197731930
Maxi Masson <dammysl222@gmail.com>
2022-08-03 11:12:58 -0300

    ejercicio 1 calcular velocidad

---
+++ Ejercicio1.py
@@ -0,0 +1,9 @@
+from tkinter import Variable
+
+
+print("ingrese la velocidad y el tiempo de la unidad móvil")
+V = int(input("Velocidad: "))
+print("ingrese el Tiempo que tarda el vehiculo")
+T = int(input("Tiempo: "))
+D = V * T 
+print(D)
